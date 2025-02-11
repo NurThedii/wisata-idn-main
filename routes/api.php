7cb41869260671d71370479d17d2732c00022099
Nur Ardi <ntterus234@gmail.com>
2025-02-11 20:44:33 +0700

    p

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
 Route::post('auth/login', [AuthController::class, 'login']);
-
-
+Route::post('auth/register', [AuthController::class, 'register']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
